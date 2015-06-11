0ce4014a092899a0525646671804dd2e2d16ce1e
amitshipra <amitg.ind@gmail.com>
2015-06-10 21:09:03 -0400

    Refactoring for more generic use. Now capable of loading individua symbols for a given exchange, a given exchange with all its symbols etc.

--- init/quandl.py
+++ init/quandl.py
@@ -1,28 +1,13 @@
 __author__ = 'Amit'
 
-import urllib2
 import Quandl
 
-def get_data():
-    # Construct the Yahoo URL with the correct integer query parameters
-    # for start and end dates. Note that some parameters are zero-based!
-    ticker = 'GOOG/BOM_532155'
-    quandl_url = "https://www.quandl.com/api/v1/datasets/%s.csv?auth_token=QydL2yFm-kAxpegBtht9" % (ticker,)
+MY_API_KEY = 'QydL2yFm-kAxpegBtht9'
 
-    # Try connecting to Yahoo Finance and obtaining the data
-    # On failure, print an error message.
-    print quandl_url
-    try:
-        yf_data = urllib2.urlopen(quandl_url).readlines()  # Ignore the header
-        prices = []
-        print('Total Rows: ' + str(len(yf_data)))
-        for y in yf_data:
-            print y.strip()
-    except Exception, e:
-        print "Could not download Quandl data: %s" % e
-        return []
+
+def get_data(q_code):
+    prices = Quandl.get(q_code)
     return prices
 
 
-mydata = Quandl.get('FRED/GDP')
-print mydata
+print(get_data("FRED/GDP"))
